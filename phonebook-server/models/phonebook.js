b57950d532334852f36405a8424d59a8a403129a
Rofi Syahrul <srofi44@gmail.com>
2019-10-18 14:30:22 +0700

    Boilerplate

---
+++ phonebook-server/models/phonebook.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const pbSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  phoneNumber: {
+    type: String,
+    match: new RegExp("^(08[0-9]{8,11})$"),
+    required: true
+  }
+});
+
+module.exports = mongoose.model("Phonebook", pbSchema);
---
+++ phonebook-server/routes/phonebook.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const Phonebook = require("../models/phonebook");
+const DataAggregation = require("../helper/DataAggregation");
+const aggregation = new DataAggregation(Phonebook);
+
+/* GET list. */
+router.get("/", (req, res) => {});
+
+module.exports = router;
