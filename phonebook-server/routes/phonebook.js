e69df3fd166068cb070081b8bc090b98129929e3
Rofi Syahrul <srofi44@gmail.com>
2019-10-18 14:45:30 +0700

    Added routers to get list of phonebook and search phonebook

--- phonebook-server/routes/phonebook.js
+++ phonebook-server/routes/phonebook.js
@@ -5,6 +5,49 @@ const DataAggregation = require("../helper/DataAggregation");
 const aggregation = new DataAggregation(Phonebook);
 
 /* GET list. */
-router.get("/", (req, res) => {});
+router.get("/", (req, res) => {
+  let limit = req.header("Limit");
+  let skip = Number(req.header("Skip") || 0);
+  aggregation
+    .countData()
+    .then(result => {
+      if (limit == "all") limit = result[0].count;
+      limit = Number(limit || result[0].count);
+      const numOfPages = Math.ceil((result[0].count || 0) / limit);
+      if (skip >= result[0].count) skip -= limit;
+      aggregation
+        .getData(limit, skip)
+        .then(data => {
+          res.status(200).json({ numOfPages, data });
+        })
+        .catch(err => console.error(err));
+    })
+    .catch(err => console.error(err));
+});
+
+// search
+router.post("/search", (req, res) => {
+  const filter = Object.keys(req.body).map(JSON.parse)[0];
+  let limit = req.header("Limit");
+  let skip = Number(req.header("Skip") || 0);
+
+  aggregation.countData(filter).then(result => {
+    let numOfPages = 0;
+    if (result[0]) {
+      if (limit == "all") limit = result[0].count;
+      limit = Number(limit || result[0].count);
+      numOfPages = Math.ceil((result[0].count || 0) / limit);
+      if (skip >= result[0].count) skip -= limit;
+    }
+    limit = Number(limit);
+
+    aggregation
+      .getData(limit, skip, filter)
+      .then(data => {
+        res.status(200).json({ numOfPages, data });
+      })
+      .catch(err => console.error(err));
+  });
+});
 
 module.exports = router;
