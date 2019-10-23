5a86dc12a487cb229276067d775d95a0c275fd23
Rofi Syahrul <srofi44@gmail.com>
2019-10-24 03:08:43 +0700

    Used constant variables to assign action's types, modified some logics, and added resend action

--- phonebook-client/src/actions/index.js
+++ phonebook-client/src/actions/index.js
@@ -1,4 +1,23 @@
 import axios from "axios";
+import Swal from "sweetalert2";
+import {
+  LOAD_DATA_SUCCESS,
+  LOAD_DATA_FAILURE,
+  ADD_DATA,
+  ADD_DATA_SUCCESS,
+  ADD_DATA_FAILURE,
+  DELETE_DATA,
+  DELETE_DATA_SUCCESS,
+  DELETE_DATA_FAILURE,
+  SEARCH_DATA,
+  SEARCH_DATA_SUCCESS,
+  SEARCH_DATA_FAILURE,
+  EDIT_DATA,
+  EDIT_ON,
+  EDIT_OFF,
+  EDIT_DATA_SUCCESS,
+  EDIT_DATA_FAILURE
+} from "../constants/actionTypes";
 
 const API_URL = "http://localhost:3001/api/phonebook/";
 
@@ -6,16 +25,16 @@ const request = axios.create({
   baseURL: API_URL,
   timeout: 1000
 });
-
 //start Loaditem from database
-
-export const loadPhonebooksSuccess = phonebooks => ({
-  type: "LOAD_PHONEBOOKS_SUCCESS",
+const loadPhonebooksSuccess = phonebooks => ({
+  type: LOAD_DATA_SUCCESS,
   phonebooks
 });
-export const loadPhonebooksFailure = () => ({
-  type: "LOAD_PHONEBOOK_FAILURE"
+
+const loadPhonebooksFailure = () => ({
+  type: LOAD_DATA_FAILURE
 });
+
 export const loadPhonebooks = () => {
   return dispatch => {
     return request
@@ -32,19 +51,19 @@ export const loadPhonebooks = () => {
 //end LoadItem
 
 //star post data
-export const addDataSuccess = (id, phonebook) => ({
-  type: "ADD_STORE_SUCCESS",
+const addDataSuccess = (id, phonebook) => ({
+  type: ADD_DATA_SUCCESS,
   id,
   phonebook
 });
 
-export const addDataFailure = id => ({
-  type: "ADD_STORE_FAILURE",
+const addDataFailure = id => ({
+  type: ADD_DATA_FAILURE,
   id
 });
 
-export const addDataRedux = (id, name, phoneNumber) => ({
-  type: "ADD_STORE",
+const addDataRedux = ({ id, name, phoneNumber }) => ({
+  type: ADD_DATA,
   id,
   name,
   phoneNumber
@@ -53,13 +72,20 @@ export const addDataRedux = (id, name, phoneNumber) => ({
 export const addStore = (name, phoneNumber) => {
   let id = Date.now();
   return dispatch => {
-    dispatch(addDataRedux(id, name, phoneNumber));
+    dispatch(addDataRedux({ id, name, phoneNumber }));
     return request
       .post("", { id, name, phoneNumber })
       .then(result => {
         let response = result.data;
-        if (response.status) dispatch(addDataSuccess(id, response.data));
-        else dispatch(addDataFailure(id))
+        if (response.status) {
+          dispatch(addDataSuccess(id, response.data));
+          Swal.fire({
+            title: response.message,
+            timer: 2000,
+            type: "success",
+            showConfirmButton: false
+          });
+        } else dispatch(addDataFailure(id));
       })
       .catch(err => {
         dispatch(addDataFailure(id));
@@ -69,28 +95,38 @@ export const addStore = (name, phoneNumber) => {
 //End Post Add
 
 //start put Edit
-
-export const editDataSuccess = phonebooks => ({
-  type: "EDIT_DATA_SUCCESS",
-  phonebooks
-});
-export const editDataFailure = id => ({
-  type: "EDIT_DATA_FAILURE",
-  id
-});
-export const editDataRedux = (id, name, phoneNumber) => ({
-  type: "EDIT_DATA",
+const editDataSuccess = ({ id, name, phoneNumber }) => ({
+  type: EDIT_DATA_SUCCESS,
   id,
   name,
   phoneNumber
 });
-export const editData = (id, name, phoneNumber) => {
+
+const editDataFailure = id => ({
+  type: EDIT_DATA_FAILURE,
+  id
+});
+
+const editDataRedux = ({ id, name, phoneNumber }) => ({
+  type: EDIT_DATA,
+  id,
+  name,
+  phoneNumber
+});
+
+export const editData = (id, name, phoneNumber, index) => {
   return dispatch => {
-    dispatch(editDataRedux(id, name, phoneNumber));
+    dispatch(editDataRedux({ id, name, phoneNumber }));
     return request
       .put(id, { name, phoneNumber })
       .then(response => {
         dispatch(editDataSuccess(response.data));
+        Swal.fire({
+          title: response.data.message,
+          type: "success",
+          timer: 1000,
+          showConfirmButton: false
+        });
       })
       .catch(err => {
         console.log(err);
@@ -99,57 +135,84 @@ export const editData = (id, name, phoneNumber) => {
   };
 };
 
+export const showEdit = (id, index) => ({ type: EDIT_ON, id, index });
+export const hideEdit = (id, index) => ({ type: EDIT_OFF, id, index });
 //end put edit
 
 //start delete deleted
-export const deletedDataSuccess = (id, phonebooks) => ({
-  type: "DELETE_STORE_SUCCESS",
-  id,
-  phonebooks
-});
-
-export const deletedDataFailure = () => ({
-  type: "DELETE_STORE_FAILURE"
-});
-
-export const deletedDataRedux = id => ({
-  type: "DELETE_STORE",
+const deleteDataSuccess = id => ({
+  type: DELETE_DATA_SUCCESS,
   id
 });
-export const deletedData = id => {
+
+const deleteDataFailure = (phonebook) => ({
+  type: DELETE_DATA_FAILURE,
+  phonebook
+});
+
+const deleteDataRedux = id => ({
+  type: DELETE_DATA,
+  id
+});
+
+export const deleteData = phonebook => {
+  const {_id, id} = phonebook 
   return dispatch => {
-    dispatch(deletedDataRedux(id));
+    dispatch(deleteDataRedux(_id || id));
     return request
-      .delete(id)
+      .delete(_id)
       .then(result => {
-        dispatch(deletedDataSuccess(id,result.data));
+        dispatch(deleteDataSuccess(_id || id));
+        Swal.fire({
+          title: "Deleted",
+          text: result.data.message,
+          type: "success",
+          timer: 2000,
+          showConfirmButton: false
+        });
       })
       .catch(err => {
         console.log(err);
-        dispatch(deletedDataFailure(id));
+        Swal.fire({
+          title: "Connection error",
+          text: "Cannot delete the contact",
+          type: "error",
+          confirmButtonClass: "btn btn-success ml-2",
+          confirmButtonText: "Retry",
+          cancelButtonClass: "btn btn-danger",
+          cancelButtonText: "Cancel",
+          reverseButtons: true,
+          showCancelButton: true
+        }).then(result => {
+          if (result.value) dispatch(deleteData(phonebook));
+          else dispatch(deleteDataFailure(phonebook));
+        });
       });
   };
 };
 //end deleted
 
 //start search
-export const searchDataSuccess = phonebooks => ({
-  type: "SEARCH_DATA_SUCCESS",
+const searchDataSuccess = phonebooks => ({
+  type: SEARCH_DATA_SUCCESS,
   phonebooks
 });
-export const searchDataFailure = () => ({
-  type: "SEARCH_DATA_FAILURE"
+
+const searchDataFailure = () => ({
+  type: SEARCH_DATA_FAILURE
 });
-export const searchDataRedux = (name, phoneNumber) => ({
-  type: "SEARCH_DATA",
+
+const searchDataRedux = (name, phoneNumber) => ({
+  type: SEARCH_DATA,
   name,
   phoneNumber
 });
+
 export const searchData = (name, phoneNumber) => {
   return dispatch => {
     dispatch(searchDataRedux(name, phoneNumber));
     return request
-      .post(`search`, { name, phoneNumber })
+      .post("search", { name, phoneNumber })
       .then(result => {
         dispatch(searchDataSuccess(result.data));
       })
@@ -162,21 +225,10 @@ export const searchData = (name, phoneNumber) => {
 //end search
 
 //start resend
-// export const resend = (id, name, phoneNumber) => {
-//   return dispatch => {
-//     dispatch(addDataRedux(id, name, phoneNumber));
-//     return request
-//       .post("", { name, message })
-//       .then(function(response) {
-//         dispatch(addDataSuccess(response.data));
-//       })
-//       .catch(function(error) {
-//         console.error(error);
-//         dispatch(addDataFailure(id));
-//       });
-//   };
-// };
+export const resendData = (id, name, phoneNumber) => {
+  return dispatch => {
+    dispatch(deleteDataRedux(id));
+    dispatch(addStore(name, phoneNumber));
+  };
+};
 // end resend
-
-export const showEdit = (id, index) => ({ type: "EDIT_ON", id, index });
-export const hideEdit = (id, index) => ({ type: "EDIT_OFF", id, index });
